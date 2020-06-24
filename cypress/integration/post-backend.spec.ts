666ebc63454fb67504f26715cf8d3193b77ec0b9
Julien FABRE <julien.fabre.33@gmail.com>
2020-06-25 01:25:27 +0200

    feat: test cypress ok

---
+++ cypress/integration/post-backend.spec.ts
@@ -0,0 +1,18 @@
+describe('Post Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/post');
+  });
+  it('don\'t call the backend', () => {
+    cy.get('i').contains('result call :');
+  });
+  it('have a trace span in console after post in the form', () => {
+    cy.get('input[name="avalue"]').as('avalue').type('test');
+    cy.get('button[type="submit"]').click();
+    cy.wait(1000);
+    cy.get('i').contains('result call : test').should(() => {
+      const value = JSON.parse(localStorage.getItem('consoleLog'));
+      expect(value.traceId).to.be.not.null;
+      expect(value.name).to.eq('http://localhost:4200/api/');
+    });
+  });
+});
