7b4c031c653da904b55f953fe264607b4b429ab6
Julien FABRE <julien.fabre.33@gmail.com>
2020-06-25 01:55:00 +0200

    feat: add cypress test e2e
    * feat: test cypress ok
    
    * chore: add cypress run on GH Action
    
    * chore: exclude cypress from jest
    
    * chore: add more time to gh

---
+++ cypress/integration/view-backend.spec.ts
@@ -0,0 +1,16 @@
+describe('View Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/view');
+  });
+  it('call the backend', () => {
+    cy.get('i').contains('result call : ok');
+  });
+  it('have a trace span in console', () => {
+    cy.wait(1000);
+    cy.get('i').should(() => {
+      const value = JSON.parse(localStorage.getItem('consoleLog'));
+      expect(value.traceId).to.be.not.null;
+      expect(value.name).to.eq('http://localhost:4200/api/');
+    });
+  });
+});
