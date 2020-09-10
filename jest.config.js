a77879a426d1e0ef5fb1b1e5b1e8994d366fcdba
Julien FABRE <julien.fabre.33@gmail.com>
2020-09-10 23:45:45 +0200

    fix: ts-jest configuration

--- jest.config.js
+++ jest.config.js
@@ -18,10 +18,12 @@ module.exports = {
     'ts-jest': {
       tsConfig: '<rootDir>/projects/opentelemetry-interceptor/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.html$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer'
-      ]
+      astTransformers: {
+        "before": [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer'
+        ]
+      }
     }
   },
   snapshotSerializers: [
