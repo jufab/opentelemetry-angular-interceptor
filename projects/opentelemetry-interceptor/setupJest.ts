203abfce2024606f667d6597176faf3630f1d934
Julien Fabre <julien.fabre.33@gmail.com>
2024-12-11 01:15:22 +0100

    fix: global on TextEncoder

--- projects/opentelemetry-interceptor/setupJest.ts
+++ projects/opentelemetry-interceptor/setupJest.ts
@@ -1,4 +1,5 @@
 import 'jest-preset-angular/setup-jest';
+import { TextEncoder } from 'util';
 /**
  * @ignore
  */
@@ -16,4 +17,8 @@ Object.defineProperty(window, 'localStorage', {value: mock()});
 Object.defineProperty(window, 'sessionStorage', {value: mock()});
 Object.defineProperty(window, 'getComputedStyle', {
   value: () => ['-webkit-appearance']
+});
+Object.defineProperty(window, 'TextEncoder', {
+  writable: true,
+  value: TextEncoder
 });
